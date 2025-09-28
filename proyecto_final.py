c4b3b9df856ba2b8d317bbd234d4a96187e5d5f5
Rommel Prieto <stiwardprieto@unicauca.edu.co>
2025-09-28 18:42:37 -0500

    Commit con escritura a la base de datos

--- proyecto_final.py
+++ proyecto_final.py
@@ -8,10 +8,19 @@ import folium
 from datetime import datetime
 import time
 import schedule
+import mysql.connector
+
+DB_HOST = 'bsoc7uhsw5gjn4zf9ves-mysql.services.clever-cloud.com'
+DB_NAME = 'bsoc7uhsw5gjn4zf9ves'
+DB_USER = 'u27hjw3jfjaengp4'
+DB_PASSWORD = 'yIUyH5vqgcH507V4HuZY'
+DB_PORT = 3306
+Connection_URI = 'mysql://u27hjw3jfjaengp4:yIUyH5vqgcH507V4HuZY@bsoc7uhsw5gjn4zf9ves-mysql.services.clever-cloud.com:3306/bsoc7uhsw5gjn4zf9ves'
+MySQL_CLI = 'mysql -h bsoc7uhsw5gjn4zf9ves-mysql.services.clever-cloud.com -P 3306 -u u27hjw3jfjaengp4 -p bsoc7uhsw5gjn4zf9ves'
 
 # API TomTom
 API_KEY = 'G1Kx7s7TNR8FRXYcF9UHD06gFOXVuzCb'
-
+API_KEY_2 = 'Q9n4jfzUJ8S32eAwzEEtkvBNGevA0Z28'
 # Puntos estratégicos de cali
 ZONAS = {
     #"Centro": (4.653, -74.083),
@@ -35,6 +44,23 @@ ZONAS = {
     "Point 9": (3.330306, -76.532753),
     "End": (3.342223, -76.530967)
 }
+
+try:
+    connection = mysql.connector.connect(
+        user = DB_USER,
+        password = DB_PASSWORD,
+        host = DB_HOST,
+        port = DB_PORT,
+        database = DB_NAME
+    )
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS trafico_cali (id INT AUTO_INCREMENT PRIMARY KEY, zona VARCHAR(50), lat FLOAT, lon FLOAT, timestamp DATETIME, velocidad_actual FLOAT, flujo_libre FLOAT, congestion FLOAT, confianza INT, distancia_ruta FLOAT, duracion_ruta FLOAT, demora_ruta FLOAT)')
+    print('Conexion exitosa')
+    cursor.close()
+    connection.close()
+except mysql.connector.Error as err:
+    print(f'Error: {err}')
+
 
 #  Función para consultar tráfico
 def obtener_velocidad(lat, lon, api_key):
@@ -95,7 +121,7 @@ def generar_dataset(zonas, api_key):
         datos_ruta = obtener_ruta(api_key)
         resultados = []
         for nombre, (lat, lon) in zonas.items():
-            datos = obtener_velocidad(lat, lon, api_key)
+            datos = obtener_velocidad(lat, lon, API_KEY_2)
             datos.update({
                 "zona": nombre,
                 "lat": lat,
@@ -106,6 +132,25 @@ def generar_dataset(zonas, api_key):
                 "demora_ruta": datos_ruta.get("demora_ruta")
             })
             resultados.append(datos)
+            # Insert data into MySQL
+            try:
+                connection = mysql.connector.connect(
+                    user = DB_USER,
+                    password = DB_PASSWORD,
+                    host = DB_HOST,
+                    port = DB_PORT,
+                    database = DB_NAME
+                )
+                cursor = connection.cursor()
+                cursor.execute(
+                    'INSERT INTO trafico_cali (zona, lat, lon, timestamp, velocidad_actual, flujo_libre, congestion, confianza, distancia_ruta, duracion_ruta, demora_ruta) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                    (nombre, lat, lon, datos['timestamp'], datos['velocidad_actual'], datos['flujo_libre'], datos['congestion'], datos['confianza'], datos_ruta.get("distancia_ruta"), datos_ruta.get("duracion_ruta"), datos_ruta.get("demora_ruta"))
+                )
+                connection.commit()
+                cursor.close()
+                connection.close()
+            except Exception as e:
+                pass
         df = pd.concat([df, pd.DataFrame(resultados)])
         cols_to_drop = [col for col in df.columns if col.startswith('Unnamed')]
         df.drop(columns=cols_to_drop, inplace=True)
@@ -144,17 +189,7 @@ def data_generation():
     except KeyboardInterrupt:
         print("\n Monitoreo detenido por el usuario.")
 # Schedule every minute
-schedule.every(1).minutes.do(lambda: data_generation())
+schedule.every(5).minutes.do(lambda: data_generation())
 while True:
     schedule.run_pending()
-    # time.sleep(1)
-# if __name__ == "__main__":
-#     try:
-#         while True:
-#             print(f"\n Sensando tráfico: {datetime.now().strftime('%H:%M:%S')}")
-#             df_trafico = generar_dataset(ZONAS, API_KEY)
-#             print(df_trafico[["zona", "velocidad_actual", "flujo_libre", "congestion", "confianza"]])
-#             generar_mapa(df_trafico)
-#             time.sleep(5)  # Espera 5 segundos antes del siguiente ciclo
-#     except KeyboardInterrupt:
-#         print("\n Monitoreo detenido por el usuario.")
+    
